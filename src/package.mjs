49224297a5b213c525e0158b07666c92d90512f8
Daniel Zenner <daniel.zenner@viaanix.com>
2023-11-16 13:52:57 -0600

    fix paths for cli

--- src/package.mjs
+++ src/package.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'node:fs';
 import { finished } from 'stream/promises';
-// import { fileURLToPath } from 'url';
+
 import {
   authFetch,
   createFile,
@@ -14,9 +14,9 @@ import {
   validatePath
 } from './utils.mjs';
 
-const __dirname = process.cwd();
+// const __dirname = process.cwd();
 // const __dirname = fileURLToPath(new URL('.', import.meta.url));
-const rootProjectPath = path.join(__dirname, '../');
+const rootProjectPath = process.cwd();
 const localWidgetPath = path.join(rootProjectPath, 'widgets');
 const scratchPath = path.join(rootProjectPath, '.bubo');
 
